5e63ebea1b6ed54b63ed87d9960884d505414285
madiscercy <madiscercy@gmail.com>
2023-05-05 13:40:36 -0400

    updating server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
+const { userRoutes, thoughtRoutes } = require('./routes');
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
 
 app.get('/', (req, res) => {
 	res.send('Hello, World!');
