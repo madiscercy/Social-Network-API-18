8631c98572830996229fac9afbdcb485113300ec
madiscercy <madiscercy@gmail.com>
2023-05-04 20:49:28 -0400

    fixed duplication

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
 const app = express();
 
@@ -7,14 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// replace DBname with database name
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/DBName', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-});
 
 app.get('/', (req, res) => {
 	res.send('Hello, World!');
